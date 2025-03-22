86e02268d700ef8781c09123566af09cb559ab37
Oskar Puchalski <oskar@p-programisci.pl>
2025-03-23 00:52:44 +0100

    refactor: Delete unnecesary files and fix npm issue

--- frontend/app/decision-explorer/page.tsx
+++
@@ -1,9 +0,0 @@
-import DecisionExplorer from "@/components/decision-explorer";
-
-export default function DecisionExplorerPage() {
-  return (
-    <main className="container mx-auto py-8">
-      <DecisionExplorer />
-    </main>
-  );
-}
